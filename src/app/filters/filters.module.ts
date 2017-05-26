85e0e78a327be7489489b31e8164e659198a74f1
Briesen <jack.briesen@sogeti.com>
2017-05-26 14:44:36 +0200

    fixed filter

--- src/app/filters/filters.module.ts
+++ src/app/filters/filters.module.ts
@@ -1,41 +1,16 @@
 import * as angular from 'angular';
 
-// export function SumOfValue(data,key) {
-//         return function(sum: number) {
-//             if (angular.isUndefined(data) || angular.isUndefined(key)) {
-//                 return sum;
-//             }
-//             else {
-//                 angular.forEach(data, function (value) {
-//                     sum = sum + parseInt(value[key]);
-//                 })
-//                 return sum;
-//             }
-//         }
-//     }
-
-module Filter {
-    'use strict';
-
-    export class SumFilter {
-
-        constructor() {
+angular.module('app').filter('sumOfValue', (data,key) => {
+    return (sum: number) => {
+        if (angular.isUndefined(data) || angular.isUndefined(key)) {
+            return sum;
         }
-
-        filter(data,key):number {
-            var result = 0;
-            if (angular.isUndefined(data)|| angular.isUndefined(key)) {
-                return result;
-            }
-            else{
-                angular.forEach(data,function(value){
-                    result =result + parseInt(value[key])
-                })
-                return result;
-            }
+        else {
+            angular.forEach(data, function (value) {
+                sum = sum + parseInt(value[key])
+            })
+            return sum;
         }
     }
-}
+});
 
-// export const FilterModule = 
-// angular.module('filter').filter('sumOfValue',Filter.SumFilter)
